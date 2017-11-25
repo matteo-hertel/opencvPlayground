7aa96cbdda64646569f4e67ebf3700f5f9b53a25
Matteo Hertel <info@matteohertel.com>
2017-11-25 12:31:45 +0000

    update index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var cv = require('opencv');
-var COLOR = [0, 255, 0]; // default red
-var thickness = 6; // default 1
+var COLOR = [0, 255, 0]; 
+var thickness = 6; 
 cv.readImage("./dummy.jpg", function(err, im){
   if (err) throw err;
   if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
@@ -10,9 +10,7 @@ cv.readImage("./dummy.jpg", function(err, im){
     for (var i = 0; i < faces.length; i++){
       var face = faces[i];
           im.rectangle([face.x, face.y], [face.width, face.height], COLOR, thickness);
-
     }
-
     im.save('./detection.jpg');
     console.log('Image saved as detection.jpg');
   });
